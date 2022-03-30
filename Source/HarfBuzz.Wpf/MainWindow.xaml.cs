e0eb1361ca8a6fa883ecc03073b16eab81f4e1a2
Mike Marynowski <mikem@singulink.com>
2022-03-30 01:36:52 -0400

    Add custom HarfBuzz test

--- Source/HarfBuzz.Wpf/MainWindow.xaml.cs
+++ Source/HarfBuzz.Wpf/MainWindow.xaml.cs
@@ -29,6 +29,8 @@ namespace HarfBuzz.Wpf
         private const string FontFace = "Arial";
         private new const float FontSize = 18;
 
+        private const int HBCustomFontScale = 512;
+
         private static readonly SKTypeface _typeFace = SKTypeface.FromFamilyName(FontFace);
         private static readonly SKFont _font = _typeFace.ToFont();
         private static readonly SKPaint _paint = new SKPaint(_font)
@@ -38,12 +40,30 @@ namespace HarfBuzz.Wpf
             Style = SKPaintStyle.Fill,
             TextAlign = SKTextAlign.Left,
             TextSize = FontSize,
+            TextEncoding = SKTextEncoding.Utf16,
         };
 
-        private static readonly SKShaper _hbShaper = new SKShaper(SKTypeface.FromFamilyName(FontFace));
+        private static readonly HB.Font _hbFont;
+        private static readonly SKShaper _hbShaper = new SKShaper(_typeFace);
 
-        private SKBitmap _swapBitmap;
-        private SKCanvas _swapCanvas;
+        private readonly HB.Buffer _hbBuffer = new();
+
+        private readonly SKBitmap _swapBitmap;
+        private readonly SKCanvas _swapCanvas;
+
+        static MainWindow()
+        {
+            using (var hbBlob = _typeFace.OpenStream(out int index).ToHarfBuzzBlob())
+            using (var hbFace = new HB.Face(hbBlob, index))
+            {
+                hbFace.Index = index;
+                hbFace.UnitsPerEm = _typeFace.UnitsPerEm;
+
+                _hbFont = new HB.Font(hbFace);
+                _hbFont.SetScale(HBCustomFontScale, HBCustomFontScale);
+                _hbFont.SetFunctionsOpenType();
+            }
+        }
 
         public MainWindow()
         {
@@ -74,7 +94,7 @@ namespace HarfBuzz.Wpf
             DrawingElement.InvalidateVisual();
         }
 
-        private void OnHarfBuzzShapingTest(object sender, RoutedEventArgs e)
+        private void OnHarfBuzzShaperTest(object sender, RoutedEventArgs e)
         {
             bool clear = ClearCanvasCheckbox.IsChecked == true;
 
@@ -105,6 +125,56 @@ namespace HarfBuzz.Wpf
                 using var textBlob = builder.Build();
 
                 // draw the text
+                _swapCanvas.DrawText(textBlob, 0, 0, _paint);
+            }
+
+            sw.Stop();
+
+            ResultText.Text = $"Elapsed time for {Iterations} iterations: {sw.ElapsedMilliseconds}ms";
+            DrawingElement.InvalidateVisual();
+        }
+
+        private void OnHarfBuzzCustomTest(object sender, RoutedEventArgs e)
+        {
+            bool clear = ClearCanvasCheckbox.IsChecked == true;
+
+            Stopwatch sw = new();
+            sw.Start();
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                if (clear)
+                    _swapCanvas.Clear(SKColors.White);
+
+                _hbBuffer.ClearContents();
+                _hbBuffer.AddUtf16(SampleText);
+                _hbBuffer.GuessSegmentProperties();
+                _hbFont.Shape(_hbBuffer);
+
+                int length = _hbBuffer.Length;
+                var glyphInfos = _hbBuffer.GlyphInfos;
+                var glyphPositions = _hbBuffer.GlyphPositions;
+
+                float textSizeY = _paint.TextSize / HBCustomFontScale;
+                float textSizeX = textSizeY * _paint.TextScaleX;
+
+                using var builder = new SKTextBlobBuilder();
+                var run = builder.AllocateHorizontalRun(_paint.ToFont(), length, FontSize);
+                var glyphs = run.GetGlyphSpan();
+                var positions = run.GetPositionSpan();
+
+                float x = 0;
+
+                for (int g = 0; g < length; g++)
+                {
+                    var pos = glyphPositions[g];
+                    glyphs[g] = (ushort)glyphInfos[g].Codepoint;
+                    positions[g] = x + pos.XOffset * textSizeX;
+
+                    x += pos.XAdvance * textSizeX;
+                }
+
+                using var textBlob = builder.Build();
                 _swapCanvas.DrawText(textBlob, 0, 0, _paint);
             }
 
